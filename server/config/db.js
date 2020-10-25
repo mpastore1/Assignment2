2446db97eb914754e03a02aa85b346001f5f981d
John Doe <marco.pastore63@gmail.com>
2020-10-25 19:30:31 -0400

    newdatabase

--- server/config/db.js
+++ server/config/db.js
@@ -3,8 +3,8 @@ Student number: 301101814
 Date 10/25/2020 */
 module.exports = {
 
-    "URI": "mongodb://localhost/assignment2"
-
+   // "URI": "mongodb://localhost/assignment2"
+    "URI": "mongodb+srv://MarcoP:pmIrzB2jPSq6CiPa@mongodbserver.lpanv.mongodb.net/assignment2?retryWrites=true&w=majority"
 }
 
 
